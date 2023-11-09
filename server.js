5999060df98a55e9db3b98149f2d217429c2c9ff
rashedevs <rasheduap2015@gmail.com>
2023-11-10 03:20:06 +0600

    database and route integration initialized

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json);
 
 //routes
 
@@ -7,6 +10,21 @@ app.get("/", (req, res) => {
   res.send("Hello from blackcoffer server");
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:12345admin@cluster0.ya7miun.mongodb.net/Blackcoffer?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Database Connected");
+    app.listen(8000, () => {
+      console.log("Server is running on port 8000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
